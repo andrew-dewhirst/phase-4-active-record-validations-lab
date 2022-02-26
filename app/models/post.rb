5c06cad7b1a8a4be7c90cd77f2e23228f96d8e36
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-02-26 18:23:37 -0500

    commit tree 85f65cb493265d18ee7be2225b93b576476feea6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+  validate :title_is_clickbait?
+
+  def title_is_clickbait?
+    title_contains = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    unless title_contains.any? { |word| title.include?(word) }
+      errors.add :title, "Need a title that includes these phrases please!"
+    end
+  end
+
 end
+
+
